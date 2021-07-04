34274bedbc6e5d5e454894a7aa60c6ae7801a29d
gnatal <guilhermenatal47@gmail.com>
2021-07-04 16:16:13 -0300

    version 1

--- versioncontroll.js
+++ versioncontroll.js
@@ -1,17 +1,25 @@
-function checkVersion(ver1, ver2) {
-  ver1 = ver1
+function checkVersion(version1, version2) {
+  const v1 = version1
     .split('.')
-    .map((s) => s.padStart(10))
+    .map((s) => s.padStart(10, 0))
     .join('.')
-  ver2 = ver2
+  const v2 = version2
     .split('.')
-    .map((s) => s.padStart(10))
+    .map((s) => s.padStart(10, 0))
     .join('.')
-  console.log('ver1', ver1)
-  console.log('ver2', ver2)
-  return ver1 <= ver2
+
+  //   console.log('versions', { version1, version2 })
+  // although I use 2 if's and else the comparision could be onlu v1 <=v2
+  if (v1 > v2) console.log('Version 1 is greater')
+  else if (v1 == v2) console.log('Version 1 is equal')
+  else console.log('Version 1 is smaller')
 }
 
-console.log(checkVersion('2.1.2', '2.1.4'))
+function testCases() {
+  checkVersion('2.1.2', '2.1.2')
+  checkVersion('1.1.2', '2.1.4')
+  checkVersion('3.1.2', '2.1.4')
+  checkVersion('2.132.2', '2.1325.4')
+}
 
-function testCases() {}
+testCases()
